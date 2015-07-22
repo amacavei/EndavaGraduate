3e8eb2911a84dda9d57c765d2acf48d298b0950c
unknown <amacavei@EN62902.endava.net>
2015-07-22 18:56:52 +0300

    TrainingErrorHandling Delete

--- TrainingErrorHandling/Exercise9/src/OverflowException.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by amacavei on 7/22/2015.
- */
-public class OverflowException extends RuntimeException {
-
-}
--- TrainingErrorHandling/Exercise9/src/UnderflowException.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by amacavei on 7/22/2015.
- */
-public class UnderflowException extends RuntimeException {
-}
