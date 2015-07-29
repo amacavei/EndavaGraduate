9f5e67168081e588fee79ffb52b55f722235df39
unknown <amacavei@EN62902.endava.net>
2015-07-29 17:21:12 +0300

    Update of training 8

--- Training-8-Threads/Exercise2/src/FilePercentage.java
+++ Training-8-Threads/Exercise2/src/FilePercentage.java
@@ -6,20 +6,26 @@ import java.util.LinkedList;
 /**
  * Created by amacavei on 7/28/2015.
  */
-public class FilePercentage {
+public class FilePercentage extends Thread{
 
-    public String word;
+    //public String word;
     public Path pathfile;
     BufferedReader rd = null;
     public LinkedList<String> split = new LinkedList<>();
     String line;
     int occ;
+    int startIndex;
+    int endIndex;
 
-    public FilePercentage(Path pathfile){
+    public FilePercentage(Path pathfile,int startIndex,int endIndex){
+
         this.pathfile = pathfile;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
     }
 
     public String[] splitString(String str){
+
         return str.split(" ");
     }
 
@@ -28,33 +34,40 @@ public class FilePercentage {
         return (o/len)*100;
     }
 
-    public void copyString() throws IOException{
+    @Override
+    public void run(){
 
         try (InputStream in = Files.newInputStream(pathfile);
              BufferedReader rd = new BufferedReader(new InputStreamReader(in))){
                 line = rd.readLine();
                 String[] words = splitString(line);
 
-                for(int i=0;i<words.length;i++) {
+                for(int i=startIndex;i<endIndex;i++) {
                     this.split.add(words[i]);
                 }
 
             for(String items:split) {
                 occ = 0;
-                for (int i = 0; i < words.length; i++) {
+                for (int i = startIndex; i < endIndex; i++) {
                      occ = wordOcc(items,words[i]);
                 }
                 System.out.printf("%s %.1f\n",items,percOcc((float)occ,(float)words.length));
             }
             System.out.println(split);
 
-        } finally {
+        }
+        catch(Exception e){}
+        finally {
             if(rd != null) {
-                rd.close();
+                try {
+                    rd.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
-    public int wordOcc(String word1,String word2){
+    public synchronized int wordOcc(String word1,String word2){
 
         if(word1.equals(word2)){
             occ++;
--- Training-8-Threads/Exercise2/src/Main.java
+++ Training-8-Threads/Exercise2/src/Main.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -8,10 +13,38 @@ import java.util.LinkedList;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException,InterruptedException {
         Path file = Paths.get("Strings.txt");
-        FilePercentage filePercentage = new FilePercentage(file);
-       // filePercentage.split = new LinkedList<>();
-        filePercentage.copyString();
+        String line;
+
+
+        ArrayList<Thread> threadlist = new ArrayList<>();
+        int threadnb = 4;
+
+        InputStream in = Files.newInputStream(file);
+        BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+            line = rd.readLine();
+            String[] words = line.split(" ");
+
+
+            for (int i = 0; i < threadnb; i++) {
+                threadlist.add(new FilePercentage(file,(i * words.length) / threadnb, ((i + 1) * words.length) / threadnb));
+            }
+            long startTime = System.nanoTime();
+            for (int i = 0; i < threadlist.size(); i++) {
+                threadlist.get(i).start();
+
+            }
+            for (int i = 0; i < threadlist.size(); i++) {
+                threadlist.get(i).join();
+            }
+            long time = System.nanoTime() - startTime;
+
+            if(rd != null) {
+                rd.close();
+            }
+
+
+        System.out.println("Threads: " + threadlist.size() + " Time: " + time/1000 + " us");
     }
 }
