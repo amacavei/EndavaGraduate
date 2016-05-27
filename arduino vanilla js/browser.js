1190762717e8631029ceb42fc7d71b86f67a57e1
Andrei Macavei <andrei.macavei@endava.com>
2016-05-27 16:07:34 +0300

    Show hide div in case of manual or automatic toggle

--- arduino vanilla js/browser.js
+++ arduino vanilla js/browser.js
@@ -4,22 +4,34 @@ socket.on('news', function (data) {
   console.log(data);
 });
 
-$('.servobtn').button();
 
-$('.servobtn').on('change',function(){
-    console.log("Setting Servo Pos:",$('input[name=servo]:checked').val())
-    socket.emit('servo',{pos:$('input[name=servo]:checked').val()});
-});
 
 $(function() {
 $('#toggle-event').change(function() {
   var checked = $(this).prop('checked');
-  console.log(checked);
-})
-})
 
-$('#ledSet').on('click',function(){
-  var tmp = parseInt($('#ledDelay').val(),10);
-  console.log("Setting LED Delay:",tmp)
-  socket.emit('led',{delay:tmp});
+  if(checked){
+
+    $(".row").show();
+
+  $('.servobtn').button();
+
+  $('.servobtn').on('change',function(){
+      console.log("Setting Servo Pos:",$('input[name=servo]:checked').val())
+      socket.emit('servo',{pos:$('input[name=servo]:checked').val()});
+  });
+
+  console.log(checked);
+
+  $('#ledSet').on('click',function(){
+    var tmp = parseInt($('#ledDelay').val(),10);
+    console.log("Setting LED Delay:",tmp)
+    socket.emit('led',{delay:tmp});
+  });
+  }else{
+    $(".row").hide();
+    console.log('automatic control');
+  }
+
+});
 });
