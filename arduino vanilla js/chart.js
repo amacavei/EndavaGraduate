30c065fc67a11cb2b3bb5ac083f801acc6c83046
Andrei Macavei <andrei.macavei@endava.com>
2016-05-27 17:45:09 +0300

    Real-time chart

--- arduino vanilla js/chart.js
+++ arduino vanilla js/chart.js
@@ -1,38 +1,55 @@
 window.onload = function () {
-  var chart = new CanvasJS.Chart("chartContainer",
-  {
-    title:{
-      text: "Output Voltage from generator"
-    },
-    animationEnabled: true,
-    axisY :{
-      includeZero: false
-    },
-    toolTip: {
-      shared: "true"
-    },
-    data: [
-    {
-      type: "spline",
-      showInLegend: true,
-      name: "Voltage",
-      markerSize: 0,
-      dataPoints: [
-      {x: new Date(2013,4,1 ), y: 430576},
-      {x: new Date(2013,4,2 ), y: 498157},
-      {x: new Date(2013,4,3 ), y: 415128},
-      {x: new Date(2013,4,4 ), y: 342031},
-      {x: new Date(2013,4,5 ), y: 320376},
-      {x: new Date(2013,4,6 ), y: 405322},
-      {x: new Date(2013,4,7 ), y: 433426},
-      {x: new Date(2013,4,8 ), y: 430876},
-      {x: new Date(2013,4,09 ), y: 372277},
-      {x: new Date(2013,4,10 ), y: 351863},
-      {x: new Date(2013,4,11 ), y: 281959},
-      {x: new Date(2013,4,12 ), y: 282666},
-      {x: new Date(2013,4,13 ), y: 353718},
-      {x: new Date(2013,4,14 ), y: 507833}
-      ]
-    }]});
-chart.render();
-}
+
+		var dps = []; // dataPoints
+
+		var chart = new CanvasJS.Chart("chartContainer",{
+			title :{
+				text: "Output Voltage"
+			},
+			data: [{
+				type: "line",
+				dataPoints: dps
+			}]
+		});
+
+		var xVal = 0;
+		var yVal = 100;
+		var updateInterval = 100;
+		var dataLength = 500; // number of dataPoints visible at any point
+
+		var updateChart = function (count) {
+			count = count || 1;
+			// count is number of times loop runs to generate random dataPoints.
+
+      $(function() {
+      $('#toggle-event').change(function() {
+        var checked = $(this).prop('checked');
+
+        if(!checked){
+
+			for (var j = 0; j < count; j++) {
+				yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+				dps.push({
+					x: xVal,
+					y: yVal
+				});
+				xVal++;
+			};
+			if (dps.length > dataLength)
+			{
+				dps.shift();
+			}
+
+			chart.render();
+
+		};
+
+		// generates first set of dataPoints
+		updateChart(dataLength);
+
+		// update chart after specified time.
+		setInterval(function(){updateChart()}, updateInterval);
+  });
+});
+
+	}
